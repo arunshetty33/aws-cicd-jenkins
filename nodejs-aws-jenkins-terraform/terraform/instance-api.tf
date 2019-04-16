f2b24bac9539a72bf0a5f7717abba5b4de5748cd
root <root@fai-blr04l1519.intl.corp.firstam.com>
2019-04-16 16:12:14 +0530

    testing

--- nodejs-aws-jenkins-terraform/terraform/instance-api.tf
+++ nodejs-aws-jenkins-terraform/terraform/instance-api.tf
@@ -1,34 +1,36 @@
 provider "aws" {
   region = "eu-west-1"
-  shared_credentials_file = "/Users/gkohli/.aws/credentials"
 }
 
 data "template_file" "api-shell-script" {
   template = "${file("scripts/user-data-api.sh")}"
+
   vars {
-    PASSWORD = "${var.RDS_PASSWORD}"
+    PASSWORD    = "${var.RDS_PASSWORD}"
     DB_HOSTNAME = "${aws_db_instance.postgres.address}"
-    PORT="${var.API_PORT}"
+    PORT        = "${var.API_PORT}"
   }
 }
 
 resource "aws_security_group" "api-instance" {
-  vpc_id = "${aws_vpc.main.id}"
-  name = "allow-ssh"
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "allow-ssh"
   description = "security group that allows all egress traffic"
+
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   ingress {
-    from_port = "${var.API_PORT}"
-    to_port = "${var.API_PORT}"
-    protocol = "tcp"
+    from_port   = "${var.API_PORT}"
+    to_port     = "${var.API_PORT}"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags {
     Name = "example-instance"
   }
