b3ee54ce8bd388ed2fcd94191c137a7cd44e0af1
root <root@fai-blr04l1519.intl.corp.firstam.com>
2019-04-16 16:18:14 +0530

    testing

--- nodejs-aws-jenkins-terraform/terraform/backend.tf
+++ nodejs-aws-jenkins-terraform/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "cicd-node-aws-jenkins-terraform"
-    key    = "node-aws-jenkins-terraform.tfstate"
+    key    = "cicd-node-aws-jenkins-terraform.tfstate"
     region = "eu-west-1"
   }
 }
