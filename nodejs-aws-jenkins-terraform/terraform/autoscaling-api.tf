52fa6ab25da0eb29e1db775c1b45b77bfb1d0072
root <root@fai-blr04l1519.intl.corp.firstam.com>
2019-05-15 20:01:19 +0530

    added gif

--- nodejs-aws-jenkins-terraform/terraform/autoscaling-api.tf
+++ nodejs-aws-jenkins-terraform/terraform/autoscaling-api.tf
@@ -17,8 +17,9 @@ resource "aws_autoscaling_group" "api-autoscaling" {
 
   vpc_zone_identifier       = ["${aws_subnet.main-public-1.id}"]
   launch_configuration      = "${aws_launch_configuration.api-launchconfig.name}"
-  min_size                  = 2
+  min_size                  = 1
   max_size                  = 2
+  desired_capacity          = 1
   health_check_grace_period = 300
   health_check_type         = "ELB"
   load_balancers            = ["${aws_elb.api-elb.name}"]
